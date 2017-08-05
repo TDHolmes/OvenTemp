d37b94ffcf726a7d3f1c34f55ae6274cddc19809
Tyler Holmes <tylerdanielholmes@gmail.com>
2017-08-05 16:11:08 -0700

    adding the initial source files and clang complete setup file for lint

---
+++ firmware/source/main.c
@@ -0,0 +1,24 @@
+/*!
+ * @file    main.c
+ * @author  Tyler Holmes
+ * @version 0.1.0
+ * @date    05-August-2017
+ * @brief   The entrypoint for the OvenTemp project.
+ *
+ */
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "common.h"
+
+
+int main(void)
+{
+    // Setup things
+
+    // The main loop
+    while (1) {
+
+    }
+}
